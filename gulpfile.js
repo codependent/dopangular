427a09846ac80d2935d1f678a13165fe6a6cd044
codependent <joseantonio.inigo@gmail.com>
2015-07-14 08:46:42 +0200

    del operations changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,39 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var del = require('del');
+var argv = require('yargs').argv;
+var isDevelopment = (argv.development === undefined) ? false : true;
 
-gulp.task('clean-css', require('del').bind(null, ['./public/css']));
+gulp.task('clean-css', del(['./public/css']));
 
-gulp.task('clean-js', require('del').bind(null, ['./public/js']));
+gulp.task('clean-js', del(['./public/js']));
 
 gulp.task('clean', ['clean-css','clean-js']);
 
 gulp.task('sass', ['clean-css'], function () {
-  gulp.src('./resources/sass/**/*.scss')
+  var stream = gulp.src('./resources/sass/**/*.scss')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass({outputStyle: 'compressed'}).on('error', plugins.sass.logError))
     .pipe(plugins.rename({extname: '.min.css'}))
-    .pipe(plugins.sourcemaps.write('./maps'))
-    .pipe(plugins.gzip({ append: false }))
-    .pipe(gulp.dest('./public/css'))
-    .pipe(plugins.livereload());
+    .pipe(plugins.sourcemaps.write('./maps'));
+  if(!isDevelopment){
+        stream = stream.pipe(plugins.gzip({ append: false }))
+    }
+    stream.pipe(gulp.dest('./public/css'))
+    stream.pipe(plugins.livereload());
 });
 
 gulp.task('compress-js', ['clean-js'], function() {
-  return gulp.src('./resources/js/**/*.js')
+  var stream = gulp.src('./resources/js/**/*.js')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.uglify())
     .pipe(plugins.rename({extname: '.min.js'}))
     .pipe(plugins.sourcemaps.write('./maps'))
-    .pipe(plugins.gzip({ append: false }))
-    .pipe(gulp.dest('./public/js'))
-    .pipe(plugins.livereload());
+  if(!isDevelopment){
+    stream.pipe(plugins.gzip({ append: false }))
+  }
+  stream.pipe(gulp.dest('./public/js'))
+  stream.pipe(plugins.livereload());
 });
  
 gulp.task('build', ['sass','compress-js']);
