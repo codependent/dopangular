30f27bac93301a28c9f00bab9ec6adcf74a8f4c9
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2016-04-25 16:14:58 +0200

    typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,1 +1,3 @@
-/// <reference path="browser\ambient\es6-shim\es6-shim.d.ts" />
+/// <reference path="browser/ambient/es6-collections/es6-collections.d.ts" />
+/// <reference path="browser/ambient/es6-promise/index.d.ts" />
+/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,1 +1,3 @@
-/// <reference path="main\ambient\es6-shim\es6-shim.d.ts" />
+/// <reference path="main/ambient/es6-collections/es6-collections.d.ts" />
+/// <reference path="main/ambient/es6-promise/index.d.ts" />
+/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
